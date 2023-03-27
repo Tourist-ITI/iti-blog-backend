43af79bb81e7776a21446712c4903a18bbbd73c6
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-03-27 18:50:01 +0200

    Resized height of image upload

--- middlewares/upload-img/upload-img.js
+++ middlewares/upload-img/upload-img.js
@@ -63,7 +63,7 @@ exports.sharpHandler = async (buffer, id) => {
   const uniqueNumber = Date.now();
   await sharp(buffer)
     .resize({
-      width:1600,
+      height:1200,
       fit: "contain",
       background: { r: 255, g: 255, b: 255, alpha: 1 },
     })
